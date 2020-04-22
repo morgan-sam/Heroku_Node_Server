2ba06c77e467a99adce133fdbd180fb8f75d6c25
Sam <samuel_morgan_@outlook.com>
2020-04-22 13:22:02 +0100

    Removed ssl flag

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_P
 	.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
-	connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-	ssl: isProduction
+	connectionString: isProduction ? process.env.DATABASE_URL : connectionString
+	// ssl: isProduction
 });
 
 module.exports = { pool };
