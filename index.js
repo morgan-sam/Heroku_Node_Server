2a4bc1fcc361d1eb6c0859e479fe2b72b39ddf47
Sam <samuel_morgan_@outlook.com>
2020-04-22 17:35:29 +0100

    Added skeleton forms/btn

--- index.js
+++ index.js
@@ -12,4 +12,36 @@ async function displayComments() {
 		document.body.innerHTML += `${el.author}&emsp;${formatDate}:<br>${el.comment}<br><br>`;
 	});
 }
+
+function addForms() {
+	document.body.innerHTML += `<form>
+	<label for="name">Name:</label><br>
+	<input type="text" id="name" name="name"><br>
+	<label for="comment">Comment:</label><br>
+	<input type="text" id="comment" name="comment">
+  	</form><br>`;
+	document.body.innerHTML += `<button type="button">Add comment</button><br><br>`;
+}
+
+async function postComment() {
+	const newComment = {
+		author: 'SNN_Bot',
+		comment: 'This is a test post'
+	};
+
+	try {
+		const response = await fetch('https://socialnetworknode.herokuapp.com/comments', {
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			method: 'post',
+			body: JSON.stringify(newComment)
+		});
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+addForms();
 displayComments();
