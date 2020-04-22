cda5eeec8280497d39018851739a5541d32b1a8d
Sam <samuel_morgan_@outlook.com>
2020-04-22 13:23:19 +0100

    Changed ssl flag to rejectUnauthorized: false

--- config.js
+++ config.js
@@ -6,8 +6,10 @@ const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_P
 	.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
-	connectionString: isProduction ? process.env.DATABASE_URL : connectionString
-	// ssl: isProduction
+	connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+	ssl: {
+		rejectUnauthorized: false
+	}
 });
 
 module.exports = { pool };
