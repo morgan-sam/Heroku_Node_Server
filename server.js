a06df89face2724d6fc02692101e518f3236c636
Sam <samuel_morgan_@outlook.com>
2020-04-22 20:54:35 +0100

    Shortened express static command

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.listen(process.env.PORT || 5000, () => {
 
 app.route('/comments').get(getComments).post(addComment);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.get('/', function(request, response) {
 	response.sendFile('./index.html', { root: __dirname });
 });
