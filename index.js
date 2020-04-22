cd4ac72a402edc9cdbff40f66cafdfeb9e5ddeea
Sam <samuel_morgan_@outlook.com>
2020-04-22 17:18:57 +0100

    Changed var name to formatDate

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ async function getComments() {
 
 async function displayComments() {
 	const comments = await getComments();
-	console.log(comments);
 	comments.forEach((el) => {
 		const jsDate = new Date(el.time).toString();
-		const date = jsDate.split(' ').slice(0, 5).join(' ');
-		document.body.innerHTML += `${el.author}&emsp;${date}:<br>${el.comment}`;
+		const formatDate = jsDate.split(' ').slice(0, 5).join(' ');
+		document.body.innerHTML += `${el.author}&emsp;${formatDate}:<br>${el.comment}`;
 	});
 }
 displayComments();
